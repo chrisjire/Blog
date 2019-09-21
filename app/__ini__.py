9bcaeee8fef98833b303247163e01fd7ae6cc671
chris <christianwilliamsjire@gmail.com>
2019-09-21 18:59:01 +0300

    this commit will import all the necessary imports to the main init file

---
+++ app/__ini__.py
@@ -0,0 +1,33 @@
+from flask import flask
+from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+bootstrap = Bootstrap()
+db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    
+    #Creating app configuration
+    app.config.from_object(config_options[config_options])
+    
+    # initializing flask extensions
+    bootstrap.init_app(app)
+    db.init_app(app)
+    login_manager.init_app(app)
+    
+    #register blueprints
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint,url_prefix = '/authenticate')
+    
+    return app
+
