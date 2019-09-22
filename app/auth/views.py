f1ad4056b0c7bcc5ce956d3bf7d3150235ebe340
chris <christianwilliamsjire@gmail.com>
2019-09-22 11:18:23 +0300

    this commit will add login methord

--- app/auth/views.py
+++ app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template,redirect,url_for,flash,request
 from . import auth
 from flask_login import login_user,logout_user,login_required
 from ..models import User
-from .forms import RegistrationForm
+from .forms import RegistrationForm, LoginForm
 from ..__ini__ import db
 
 @auth.route('/register',methods = ["GET","POST"])
@@ -19,3 +19,17 @@ def register():
         return redirect(url_for('auth.login'))
         title = "New Account"
     return render_template('auth/register.html',form =form)
+
+@auth.route('/login',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email = form.email.data).first()
+        if user is not None and user.verify_password(form.password.data):
+            login_user(user,form.remember.data)
+            return redirect(request.args.get('next') or url_for('main.index'))
+
+        flash('Invalid username or Password')
+
+    title = "Login"
+    return render_template('auth/login.html',form =form,title=title)
