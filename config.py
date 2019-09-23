cde8cae37d0185f016f2e6cd9d2e08167fda3d0c
chris <christianwilliamsjire@gmail.com>
2019-09-23 20:06:57 +0300

    fix error

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://chris:matthewwilliams@localhost/blog'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://chris:matthewwilliams@localhost/blog'
     UPLOADED_PHOTOS_DEST = 'app/static/photos'
     QUOTES_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
     MAIL_SERVER = 'smtp.googlemail.com'
